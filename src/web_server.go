895751b4f7c69d6413814c54ade195371f87d6f7
QDSN <stagcutlery@gmail.com>
2021-12-18 01:18:56 +0900

    base

---
+++ src/web_server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "要素追加")
+}
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
